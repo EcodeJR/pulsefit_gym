447216f24918313b432c26f7ed584570fd03baf8
EcodeJR <emmanueldcode@gmail.com>
2024-07-04 11:10:00 +0100

    pulse created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section className='w-full h-screen bg-black'>
+        <div className="w-full h-[10vh] bg-white flex items-center justify-center">
+          <h1 className="text-2xl text-red-600 font-bold">PulseFit Gym</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
